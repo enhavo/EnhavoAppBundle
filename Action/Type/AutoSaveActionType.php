fbecea02ace82da55de4ddec31f4ff5298e95121
Gerhard Seidel <gseidel.message@googlemail.com>
2024-09-04 12:24:58 +0200

    refactor: preview and form integration

--- Action/Type/AutoSaveActionType.php
+++ Action/Type/AutoSaveActionType.php
@@ -1,37 +1,49 @@
 <?php
+/**
+ * PreviewButton.php
+ *
+ * @since 29/05/16
+ * @author gseidel
+ */
 
 namespace Enhavo\Bundle\AppBundle\Action\Type;
 
 use Enhavo\Bundle\ApiBundle\Data\Data;
 use Enhavo\Bundle\ResourceBundle\Action\AbstractActionType;
+use Enhavo\Bundle\ResourceBundle\Action\Type\SaveActionType;
 use Enhavo\Bundle\ResourceBundle\Model\ResourceInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class DownloadActionType extends AbstractActionType
+class AutoSaveActionType extends AbstractActionType
 {
     public function createViewData(array $options, Data $data, ResourceInterface $resource = null): void
     {
-        $data->set('ajax', $options['ajax']);
+        $data->set('timeout', $options['timeout']);
+        $data->set('iconInactive', $options['icon_inactive']);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'label' => 'label.download',
+            'label' => 'label.auto_save',
             'translation_domain' => 'EnhavoAppBundle',
-            'icon' => 'file_download',
-            'ajax' => false,
-            'model' => 'DownloadAction',
+            'icon' => 'sync',
+            'icon_inactive' => 'sync_disabled',
+            'route' => null,
+            'route_parameters' => [],
+            'model' => 'AutoSaveAction',
+            'component' => 'action-auto-save',
+            'timeout' => 5,
         ]);
     }
 
     public static function getParentType(): ?string
     {
-        return UrlActionType::class;
+        return SaveActionType::class;
     }
 
     public static function getName(): ?string
     {
-        return 'download';
+        return 'auto_save';
     }
 }
